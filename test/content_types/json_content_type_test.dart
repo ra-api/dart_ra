415c68d402852f4d67986e0135c12aa02c72107c
apohodun <apohodun@sdvor.com>
2024-02-25 19:11:56 +0500

    JsonContentType tests,

---
+++ test/content_types/json_content_type_test.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:mab/mab.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('JsonContentType tests', () {
+    final contentType = JsonContentType();
+
+    test('Check mime-type', () {
+      expect(contentType.mimeType, equals('application/json'));
+    });
+
+    test('Success apply', () {
+      final json = {'foo': 'bar'};
+      final jsonBytes = utf8.encode(jsonEncode(json));
+      final res = contentType.apply(json, null);
+      expect(res, equals(jsonBytes));
+    });
+  });
+}
